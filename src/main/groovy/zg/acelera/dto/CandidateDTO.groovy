1fe3e1fc44e00886b660fb4bd47bb344de5abe5c
Gildesio-af <gildesioaf11@gmail.com>
2026-08-17 14:55:42 -0300

    fix: fixing call of internal properties of the DTOs

--- src/main/groovy/zg/acelera/dto/CandidateDTO.groovy
+++ src/main/groovy/zg/acelera/dto/CandidateDTO.groovy
@@ -2,29 +2,30 @@ package zg.acelera.dto
 
 import groovy.transform.builder.Builder
 import zg.acelera.domain.Candidate
+import zg.acelera.domain.Skill
 
 @Builder
 record CandidateDTO(String cpf, String name, String email, Integer age, String state, String cep, String description, Set<String> skills) {
     public CandidateDTO {
-        if (cpf() == null || cpf().trim().isEmpty() || cpf().length() != 11)
+        if (cpf == null || cpf.trim().isEmpty() || cpf.length() != 11)
             throw new IllegalArgumentException("Please provide a valid CPF with 11 digits.")
-        if (name() == null || name().trim().isEmpty())
+        if (name == null || name.trim().isEmpty())
             throw new IllegalArgumentException("Please provide a valid name.")
-        if (email() == null || email().trim().isEmpty())
+        if (email == null || email.trim().isEmpty())
             throw new IllegalArgumentException("Please provide a valid email.")
-        if (age() == null || age() < 0)
+        if (age == null || age < 0)
             throw new IllegalArgumentException("Please provide a valid age.")
-        if (state() == null || state().trim().isEmpty())
+        if (state == null || state.trim().isEmpty())
             throw new IllegalArgumentException("Please provide a valid state.")
-        if (cep() == null || cep().trim().isEmpty() || cep().length() != 8)
+        if (cep == null || cep.trim().isEmpty() || cep.length() != 8)
             throw new IllegalArgumentException("Please provide a valid CEP.")
-        if (description() == null || description().trim().isEmpty())
+        if (description == null || description.trim().isEmpty())
             throw new IllegalArgumentException("Please provide a valid description.")
-        if (skills() == null || skills().isEmpty())
+        if (skills == null || skills.isEmpty())
             throw new IllegalArgumentException("Please provide a valid set of skills.")
     }
 
-    toCandidate() {
+    Candidate toCandidate() {
         return Candidate.builder()
                 .cpf(cpf)
                 .name(name)
@@ -33,7 +34,7 @@ record CandidateDTO(String cpf, String name, String email, Integer age, String s
                 .state(state)
                 .cep(cep)
                 .description(description)
-                .skills(skills.collect { zg.acelera.domain.Skill.valueOf(it) } as HashSet)
+                .skills(skills.collect { Skill.valueOf(it) } as HashSet)
                 .build()
     }
 }
--- src/main/groovy/zg/acelera/dto/CandidateUpdateDTO.groovy
+++ src/main/groovy/zg/acelera/dto/CandidateUpdateDTO.groovy
@@ -14,29 +14,29 @@ record CandidateUpdateDTO (
         Set<String> skills
 ) {
     public CandidateUpdateDTO {
-        if (name() != null && name().trim().isEmpty())
+        if (name != null && name.trim().isEmpty())
             throw new IllegalArgumentException("Please provide a valid name.")
-        if (email() != null && email().trim().isEmpty())
+        if (email != null && email.trim().isEmpty())
             throw new IllegalArgumentException("Please provide a valid email.")
-        if (age() != null && age() < 0)
+        if (age != null && age < 0)
             throw new IllegalArgumentException("Please provide a valid age.")
-        if (state() != null && state().trim().isEmpty())
+        if (state != null && state.trim().isEmpty())
             throw new IllegalArgumentException("Please provide a valid state.")
-        if (cep() != null && (cep().trim().isEmpty() || cep().length() != 8))
+        if (cep != null && (cep.trim().isEmpty() || cep.length() != 8))
             throw new IllegalArgumentException("Please provide a valid CEP.")
-        if (description() != null && description().trim().isEmpty())
+        if (description != null && description.trim().isEmpty())
             throw new IllegalArgumentException("Please provide a valid description.")
-        if (skills() != null && skills().isEmpty())
+        if (skills != null && skills.isEmpty())
             throw new IllegalArgumentException("Please provide a valid set of skills.")
     }
 
     Candidate updateCandidate(Candidate candidate) {
-        if (name() != null) candidate.name = name()
-        if (email() != null) candidate.email = email()
-        if (age() != null) candidate.age = age()
-        if (state() != null) candidate.state = state()
-        if (cep() != null) candidate.cep = cep()
-        if (description() != null) candidate.description = description()
+        if (name() != null) candidate.name = name
+        if (email() != null) candidate.email = email
+        if (age() != null) candidate.age = age
+        if (state() != null) candidate.state = state
+        if (cep() != null) candidate.cep = cep
+        if (description() != null) candidate.description = description
         if (skills() != null) candidate.skills = skills().collect { Skill.valueOf(it) } as HashSet
         candidate
     }
