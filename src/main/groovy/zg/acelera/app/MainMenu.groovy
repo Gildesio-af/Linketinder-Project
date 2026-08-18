15aaf791b2f2ef6343219096091fc3af61fa5cf3
Gildesio-af <gildesioaf11@gmail.com>
2026-08-18 09:03:34 -0300

    feature: added method to company menu

--- src/main/groovy/zg/acelera/app/MainMenu.groovy
+++ src/main/groovy/zg/acelera/app/MainMenu.groovy
@@ -1,13 +1,19 @@
 package zg.acelera.app
 
 import zg.acelera.service.CandidateService
+import zg.acelera.service.CompanyService
 import zg.acelera.user_interface.CandidateUI
+import zg.acelera.user_interface.CompanyUI
 import zg.acelera.user_interface.InputReader
 
 class MainMenu {
     static InputReader inputReader = new InputReader()
+
     static CandidateService candidateService = new CandidateService()
     static CandidateUI candidateUI = new CandidateUI(candidateService, inputReader)
+
+    static CompanyService companyService = new CompanyService()
+    static CompanyUI companyUI = new CompanyUI(companyService, inputReader)
 
     static void startApplication() {
         boolean running = true
@@ -18,7 +24,7 @@ class MainMenu {
                  LINKETINDER - MAIN MENU
             ========================================
             1. Candidate Area
-            2. Company Area (TODO)
+            2. Company Area
             0. Exit System
             ========================================
             """
@@ -29,7 +35,7 @@ class MainMenu {
                     candidateUI.showMenu()
                     break
                 case "2":
-                    println("As soon as possible, the company area will be available. Stay tuned!")
+                    companyUI.showMenu()
                     break
                 case "0":
                     println "Exiting the system. Goodbye!"
