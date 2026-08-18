b99f57c86a04a09a33f388d7e6d879577705d9c9
Gildesio-af <gildesioaf11@gmail.com>
2026-08-18 08:24:37 -0300

    feature: create repository and service for Company

---
+++ src/main/groovy/zg/acelera/repository/ICompanyRepository.groovy
@@ -0,0 +1,16 @@
+package zg.acelera.repository
+
+import zg.acelera.domain.Company
+import zg.acelera.domain.IPerson
+import zg.acelera.domain.Skill
+import zg.acelera.dto.company.CompanyDTO
+import zg.acelera.dto.company.CompanyUpdateDTO
+
+interface ICompanyRepository {
+    List<IPerson> findAll()
+    IPerson findByCnpj(String cnpj)
+    List<IPerson> findBySkill(Skill skill)
+    Company save(CompanyDTO user)
+    Company update(CompanyUpdateDTO user)
+    void delete(String cnpj)
+}
