ff299bda95c5df98dd6c8d5944b256fe815fc148
Gildesio-af <gildesioaf11@gmail.com>
2026-08-21 22:17:13 -0300

    test: add unit tests for CandidateService registration methods

---
+++ src/test/groovy/service/CandidateServiceSpec.groovy
@@ -0,0 +1,45 @@
+package service
+
+import spock.lang.Specification
+import zg.acelera.domain.Skill
+import zg.acelera.dto.candidate.CandidateDTO
+import zg.acelera.repository.ICandidateRepository
+import zg.acelera.service.CandidateService
+
+class CandidateServiceSpec extends Specification {
+
+    def "registerCandidates should register a new candidate"() {
+        given:
+        CandidateDTO newCandidate = CandidateDTO.builder()
+                .cpf("12345678900")
+                .name("Zoro")
+                .email("zorop@gmail.com")
+                .age(20)
+                .state("GL")
+                .cep("12345678")
+                .description("Member of the Straw Hat crew")
+                .skills(Set.of(Skill.JAVA, Skill.GO, Skill.CSHARP))
+                .build()
+
+        ICandidateRepository candidateRepository = Mock()
+        CandidateService service = new CandidateService(candidateRepository)
+
+        when:
+        service.registerCandidate(newCandidate)
+
+        then:
+        1 * candidateRepository.save(newCandidate)
+    }
+
+    def "registerCandidate should do nothing when candidate is null"() {
+        given:
+        ICandidateRepository candidateRepository = Mock()
+        CandidateService service = new CandidateService(candidateRepository)
+
+        when:
+        service.registerCandidate(null)
+
+        then:
+        notThrown(Exception)
+    }
+}
